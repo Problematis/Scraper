8f20f6317b57b9e05e2323d5bb5cb639a060f5fa
Problematis <peter.hancock@selestaltd.co.uk>
2018-04-06 13:15:58 +0100

    t

--- scraper.py
+++ scraper.py
@@ -1,4 +1,27 @@
-import requests
+import urllib.request
+from bs4 import BeautifulSoup
 
-response_object = requests.get('http://edmundmartin.com')
-print(response_object.text)
+url = "https://www.reddit.com/top"
+# download the URL and extract the content inot the variable html
+request = urlib.request.Request(url)
+html = urllib.request.urlopen(request).read()
+
+#pass the HTML to Beautifulsoup.
+soup = BeautifulSoup(html,'html.parer')
+#get the HTML of the table called site Table where all the links are displayed
+main_table = soup.find("div",attrs={'id':'siteTable'})
+#Now we go into main_table and get every a element in it which has a class "title" 
+links = main_table.find_all("a",class_="title")
+
+#from each link extract the text of link and the link itself
+#List to store a dict of the data we extracted 
+extracted_records = []
+for link in links: 
+    title = link.textT
+    url = link['href']
+    record = {
+        'title':title
+        'url':url
+        }
+    extracted_records.append(record)
+print(extracted_records)
